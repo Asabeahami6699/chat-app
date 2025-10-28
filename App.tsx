9f2658e26018d491b46078f79e52321eb7301ef4
ProtocolGh <protocolgh6699@gmail.com>
2025-10-28 11:04:58 -1200

    feat: full chat app with video calls

---
+++ App.tsx
@@ -0,0 +1,5 @@
+﻿import { NavigationContainer } from '@react-navigation/native';
+import TabNavigator from './src/navigation/TabNavigator';
+export default function App() {
+  return <NavigationContainer><TabNavigator /></NavigationContainer>;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,1 @@
+﻿module.exports = { content: ['./App.{js,tsx}', './src/**/*.{js,tsx}'], theme: { extend: {} }, plugins: [] };
